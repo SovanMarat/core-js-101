adf003dd43556a046179ea3ee24d46e8b6773e55
SovanMarat <maratwp@mail.ru>
2020-12-11 22:58:03 +0300

    feat: 36  done

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -250,8 +250,14 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  const res = [];
+  arr.reduce((prev, item) => {
+    res.push(prev + item);
+
+    return prev + item;
+  }, 0);
+  return res;
 }
 
 /**
